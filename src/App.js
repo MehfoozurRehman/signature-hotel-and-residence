c56f62f5b2bfae4622a8178cd98aee26aad5aa8e
Mehfooz-ur-Rehman <mehfoozijaz786@gmail.com>
2021-10-01 10:21:14 +0500

    ran npx browserslist@latest --update-db and added routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React, { Component } from "react";
-
+import React, { Component, lazy, Suspense } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+const HomeScreen = lazy(() => import("Screens/HomeScreen"));
 export default class App extends Component {
   render() {
-    return <div>hello</div>;
+    return (
+      <Suspense fallback={"hello"}>
+        <Router>
+          <Switch>
+            <Route>
+              <HomeScreen />
+            </Route>
+          </Switch>
+        </Router>
+      </Suspense>
+    );
   }
 }
