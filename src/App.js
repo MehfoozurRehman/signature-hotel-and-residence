c55b4bb5758467ede87e7b0df33c78a840372934
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2021-10-05 00:54:23 +0500

    partially done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component, lazy, Suspense } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-const HomeScreen = lazy(() => import("Screens/HomeScreen"));
+const HomeScreen = lazy(() => import("./Screens/HomeScreen"));
 export default class App extends Component {
   render() {
     return (
